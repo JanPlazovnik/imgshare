edb890496d1965da3748defdfddec07ce57eb125
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-09-15 20:22:48 +0200

    Made changes to the code

--- components/nav.php
+++ components/nav.php
@@ -3,10 +3,10 @@
         <a class="active" href="index.php">Imgix</a>
         <a href="upload.php">Upload</a>
         <?php if($_SESSION['logged_in'] == true): ?>
-            <a style="float: right" href="logout.php">Sign out</a>
+            <a class="right" href="logout.php">Sign out</a>
         <?php else: ?>
-            <a style="float: right" href="login.php">Sign in</a>
-            <a style="float: right" href="register.php">Register</a>
+            <a class="right" href="login.php">Sign in</a>
+            <a class="right" href="register.php">Register</a>
         <?php endif ?>
     </div>
 </div>
