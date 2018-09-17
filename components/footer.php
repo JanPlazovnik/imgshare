b47aaba8ca79fa2f5856c8f19d96407fc2ddc72d
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-09-17 19:40:54 +0200

    Changed footer text

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-    <p class="footer-author">Made with <span class="footer-heart">♥</span> by a filthy noob &copy; 2018</p>
+    <p class="footer-author">Made with <span class="footer-heart">♥</span> by Jan &copy; 2018</p>
 </div>
