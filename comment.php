620aa37aea5b3a7043526e3f0885b65fb3ba84b4
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-09-30 13:36:59 +0200

    Delete comment.php

--- comment.php
+++
@@ -1,9 +0,0 @@
-echo "
-                            <div class='comment-item'>
-                                <div class='comment-author'>
-                                    <p><a class='img-url' href='user.php?user=$commenter'>$commenter</a> on $when</p>
-                                </div>
-                                <div class='comment-content'>
-                                    <p>" . $row['comment_text'] . "</p>
-                                </div>
-                            </div>";
