f1befd1163f1f13003644a5f487f406eb0dc237f
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-10-12 18:33:51 +0200

    Added a console logging function

--- app/db.php
+++ app/db.php
@@ -1,3 +1,12 @@
 <?php
     $mysqli = new mysqli('localhost', 'root', 'mysql', 'imgsite') or die($mysqli->error);
+
+    function debug_to_console( $data ) {
+        $output = $data;
+        if ( is_array( $output ) )
+            $output = implode( ',', $output);
+    
+        echo "<script>console.log( 'Debug: " . $output . "' );</script>";
+    }
+
 ?>
