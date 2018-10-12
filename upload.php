f25f8b17f0622cf3917b00736565ed6e62dcb69a
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-10-12 18:35:55 +0200

    Fixed a bug, modified error echoing
    Fixed a bug where refreshing the page would resubmit form data

--- upload.php
+++ upload.php
@@ -30,36 +30,32 @@
 
     $uploadPath = $currentDir . $uploadDirectory . basename($fileName); 
 
-    if (isset($_POST['submit'])) {
-
-        if (! in_array($fileExtension,$fileExtensions)) {
-            $errors[] = "This file extension is not allowed. Please upload a .jpeg, .jpg, .png or .gif format.";
+    if(isset($_POST['submit'])) 
+    {
+        if(!in_array($fileExtension, $fileExtensions)) {
+            $errors[] = "Invalid format. Use only .jpeg, .jpg, .png or .gif format.";
         }
-
-        if ($fileSize > 2000000) {
-            $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+        if($fileSize > 2000000) {
+            $errors[] = "File exceeds the maximum size limit of 2MB.";
         }
-
-        if (empty($errors)) {
+        else {
             $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
-
-            if ($didUpload) {
+            if ($didUpload) 
+            {
                 echo "The file " . basename($fileName) . " has been uploaded";
                 rename('images/' . basename($fileName), 'images/' . $imagehash . "." . $fileExtension);
                 $sql = "INSERT INTO images (user_id, imagehash, extension, image_title, image_description, time_uploaded)". "VALUES ('{$_SESSION['user_id']}', '$imagehash', '$fileExtension', '$title', '$description', NOW())";  
                 if($mysqli->query($sql))
-                {
-                    echo "<p>DB Successful</p>";
+                {   
                     $imgurl = "gallery.php?img=" . $imagehash;
-                } else {
-                    echo "<p>DB Error</p>";
+                    header("location: " . $imgurl);
+                } 
+                else {
+                    $errors[] = "Couldn't execute the query.";
                 }               
-            } else {
-                echo "An error occurred somewhere. Try again or contact the admin";
-            }
-        } else {
-            foreach ($errors as $error) {
-                echo $error . "These are the errors" . "\n";
+            } 
+            else {
+                $errors[] = "An error occurred somewhere. Try again or contact the admin";
             }
         }
     }
@@ -84,9 +80,15 @@
             <input class="input" type="text" placeholder="Description" name="description" />       
             <button class="button" type="submit" name="submit">Upload</button>
         </form>
-        <?php if(isset($imgurl)){ ?><span class="img-url"><?php echo "You can find your image <a href='" . $imgurl . "'>here</a>"; ?></span><?php } ?>
         <?php if(isset($errmsg)){ ?><span><?php echo $errmsg; ?></span><?php } ?>
         <?php if(isset($succmsg)){ ?><span><?php echo $succmsg; ?></span><?php } ?>
+        <?php if(!empty($errors))
+        {
+            foreach ($errors as $error) {
+                echo "<p class='error'>" . $error . "</p>";
+            }
+        }
+        ?>
     </div>
     <?php require 'components/footer.php' ?>
 </body>
