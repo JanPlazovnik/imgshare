734eebb0046bc2aafcfed15b8f03f8074cf19ff3
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-10-23 21:59:04 +0200

    Added inputs for email and password

--- settings.php
+++ settings.php
@@ -2,13 +2,13 @@
     require 'app/db.php';
     session_start();
 
-    if($_SESSION['logged_in'] == true)
+    if(!$_SESSION['logged_in'])
     {
         header("location: index.php");
     }
 
-    $ID = $_SESSION['user_id'];
-    $sql = $mysqli->query("SELECT * FROM users WHERE id='$ID");
+    $id = $_SESSION['user_id'];
+    $sql = $mysqli->query("SELECT * FROM users WHERE id='$id'") or trigger_error("ERROR:" . mysqli_error($mysqli), E_USER_ERROR);;
     $user = $sql->fetch_assoc();
 ?>
 <!DOCTYPE html>
@@ -22,7 +22,26 @@
 </head>
 <body>
     <?php require 'components/nav.php' ?>
-    <div class="container"> 
+    <div class="center">
+        <?php if(isset($_GET['success'])):?>
+        <?php if($_GET['success'] == 1):?>
+        <div id="success">
+            <p>Changes saved.</p>
+        </div>
+        <?php endif?>
+        <?php if($_GET['success'] == 0):?>
+        <div id="error">
+            <p>Changes not saved.</p>
+        </div>
+        <?php endif ?>
+        <?php endif ?>
+        <form action="update-settings.php" method="post" autocomplete="off" enctype="multipart/form-data">
+            <p>Email</p>
+            <input value="<?php echo $user['email'] ?>" type="text" name="email"/>
+            <label>Password</label>
+            <input type="password" name="password"/>
+            <button type="submit" name="save">Save settings</button>
+        </form>
     </div>
     <?php require 'components/footer.php' ?>
 </body>
