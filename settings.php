67308f4d717fd7d51f030dd9636a29b819641dda
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-10-27 20:11:46 +0200

    Complete remake of the settings page

--- settings.php
+++ settings.php
@@ -23,26 +23,46 @@
 <body>
     <?php require 'components/nav.php' ?>
     <div class="center">
-        <?php if(isset($_GET['success'])):?>
-        <?php if($_GET['success'] == 1):?>
-        <div id="success">
-            <p>Changes saved.</p>
+        <div class="settings">
+            <div class="settings-menu noselect">
+                <div class="settings-email inlblock settings-item">
+                    <span><i class="icofont-email"></i> Email</span>
+                </div>
+                <div class="settings-password inlblock settings-item">
+                    <span><i class="icofont-ui-password"></i> Password</span>
+                </div>
+            </div>
+            <div class="settings-content">
+                <div class="settings-content-email">
+                    <form action="settings-email.php" method="post" autocomplete="off" enctype="multipart/form-data">
+                        <p>Enter your new email</p>
+                        <input class="input" type="text" name="email" placeholder="Email" required>
+                        <button class="button" type="submit" name="submitEmail">Submit</button>
+                    </form>
+                </div>
+                <div class="settings-content-password settings-form-hidden">
+                    <form action="settings-password.php" method="post" autocomplete="off" enctype="multipart/form-data">
+                        <p>Enter your current password</p>
+                        <input class="input" type="password" name="currentPassword" placeholder="Old password" required>
+                        <p>Enter your new password</p>
+                        <input class="input" type="password" name="newPassword" placeholder="New password" required>
+                        <button class="button" type="submit" name="submitPassword">Submit</button>
+                    </form>                   
+                </div>
+            </div>
         </div>
-        <?php endif?>
-        <?php if($_GET['success'] == 0):?>
-        <div id="error">
-            <p>Changes not saved.</p>
-        </div>
-        <?php endif ?>
-        <?php endif ?>
-        <form action="update-settings.php" method="post" autocomplete="off" enctype="multipart/form-data">
-            <p>Email</p>
-            <input value="<?php echo $user['email'] ?>" type="text" name="email"/>
-            <label>Password</label>
-            <input type="password" name="password"/>
-            <button type="submit" name="save">Save settings</button>
-        </form>
     </div>
+    <script type="text/javascript">
+    $(".settings-email").on("click", function(){
+        $(".settings-content-email").removeClass("settings-form-hidden");
+        $(".settings-content-password").addClass("settings-form-hidden");
+    });
+    $(".settings-password").on("click", function(){
+        $(".settings-content-password").removeClass("settings-form-hidden");
+        $(".settings-content-email").addClass("settings-form-hidden");    
+    });
+    </script>
     <?php require 'components/footer.php' ?>
 </body>
 </html>
+
