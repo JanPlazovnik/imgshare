febfcb87886a9707282f0f47ea8df8a4eaccee02
Jan Plazovnik <jan.plazovnik@gmail.com>
2018-11-04 13:12:52 +0100

    Added meta

--- components/head.php
+++ components/head.php
@@ -1,3 +1,10 @@
+<?php
+    if(!isset($title)) $title = 'Imgix';
+    if(!isset($desc)) $desc = 'Upload and share your photos.';
+?>
+<meta content="Imgix" property="og:site_name">
+<meta content="<?php echo $title?>" property="og:title">
+<meta content="<?php echo $desc?>" property="og:description">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <link href="css/style.css" rel="stylesheet"/>
 <link href="css/icofont.css" rel="stylesheet">
